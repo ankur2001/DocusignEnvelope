2cc63c961b991266286c13cb9bc1ba648d1904ea
ankur2001 <33401101+ankur2001@users.noreply.github.com>
2019-12-05 14:01:14 -0800

    final docusign code

--- src/app.js
+++ src/app.js
@@ -7,15 +7,20 @@ api.get('/hello', function () {
 });
 
 api.get('/liveStatus', function () {	
-	var Status = require('./src/controllers/Status');	
+	var Status = require('./controllers/Status');	
 	var finalResponse = Status.getLiveStatus()
   return finalResponse;
 });
 
-api.get('/sendEnvelope', function () {	
-console.log("inside app.js under sendEnvelope")
-	var Docusign = require('./controllers/Docusign');	
-	var finalResponse = Docusign.sendEnvelope()
+api.post('/sendEnvelope', function (req,res,next) {	
+	console.log("inside app.js under sendEnvelope")
+	console.log("Request received at sendEnvelope", req)
+	console.log("Request body received at sendEnvelope", req.body)
+	console.log("Request body email received at sendEnvelope", req.body.email)
+	const emailId = req.body.email;
+	var Docusign = require('./service/docusignService');	
+	var finalResponse = Docusign.sendEnvelopeController(emailId);
+	console.log("inside app.js final response", finalResponse)
 	return finalResponse.then(function(res){
 		  console.log("response in app")
 		  console.log("response in app.2",res)
@@ -26,5 +31,24 @@ console.log("inside app.js under sendEnvelope")
 		  console.log("error in app.2",err)
 		  return err;
 	  })
-//  return finalResponse;
+});
+
+
+api.get('/envStatus', function (req, res, next) {	
+	console.log("inside app.js under envStatus")
+	console.log("req.queryString in get Envelope Status", req.queryString)
+	console.log("req.queryString.envelopeId in get Envelope Status", req.queryString.envelopeId)
+	const envelopeId = req.queryString.envelopeId;
+	var DocusignEnv = require('./service/docusignAPICall');
+	var finalResponse = DocusignEnv.envStatus(envelopeId);
+	 return finalResponse.then(function(res){
+		  console.log("response in app")
+		  console.log("response in app.1",res)
+		  console.log("response in app.1.1",res.data)
+		  return res;		  
+	  }).catch(function(err){
+		  console.log("error in app")
+		  console.log("error in app.1",err)
+		  return err;
+	  })
 });
