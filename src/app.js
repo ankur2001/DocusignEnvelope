197f219989d1bbf7bbf440829df42443b6619b63
ankur2001 <33401101+ankur2001@users.noreply.github.com>
2019-12-05 12:44:01 -0800

    sendEnv changes

--- src/app.js
+++ src/app.js
@@ -6,11 +6,25 @@ api.get('/hello', function () {
   return 'hello world';
 });
 
-api.get('/liveStatus', function () {
-	
-	var Status = require('./controllers/Status');
-	
+api.get('/liveStatus', function () {	
+	var Status = require('./src/controllers/Status');	
 	var finalResponse = Status.getLiveStatus()
-
   return finalResponse;
+});
+
+api.get('/sendEnvelope', function () {	
+console.log("inside app.js under sendEnvelope")
+	var Docusign = require('./controllers/Docusign');	
+	var finalResponse = Docusign.sendEnvelope()
+	return finalResponse.then(function(res){
+		  console.log("response in app")
+		  console.log("response in app.2",res)
+		  console.log("response in app.2.2",res.data)
+		  return res;		  
+	  }).catch(function(err){
+		  console.log("error in app")
+		  console.log("error in app.2",err)
+		  return err;
+	  })
+//  return finalResponse;
 });
--- src/controllers/Docusign.js
+++ src/controllers/Docusign.js
@@ -1,8 +1,22 @@
 'use strict';
 var Docusign = require('../service/docusignService');
 
-module.exports.sendEnvelope = function sendEnvelope (req, res, next) {
+module.exports.sendEnvelope = function sendEnvelope () {
+	
+	return new Promise(function (resolve, reject){
 	  var serviceRes = Docusign.sendEnvelopeController('ankur2001@gmail.com')
-	  return serviceRes;
+	  //return serviceRes;
+	  console.log ("serviceRes inside controller", JSON.stringify(serviceRes))
+	  serviceRes.then(function(res){
+		  console.log("response in app")
+		  console.log("response in app.2.1.1",res)
+		  console.log("response in app.2.1.1.1.1",res.data)
+		  return res;		  
+	  }).catch(function(err){
+		  console.log("error in app 2.2.2")
+		  console.log("error in app.2.2.2.2",err)
+		  return err;
+	  })
+	})
     
 };
